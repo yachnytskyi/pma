36d6a3e70d98830a4675baca173a94ae7ee4f21d
yachnytskyi <yachnytskyi1992@gmail.com>
2018-10-02 07:46:24 +0300

    Created scaffold Teams and import style Teams to application.css.scss file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_043514) do
+ActiveRecord::Schema.define(version: 2018_10_02_044311) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
