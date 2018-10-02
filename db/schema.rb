eead01c56ba564c7a62b425eb6f6bd9c9376a3cd
yachnytskyi <yachnytskyi1992@gmail.com>
2018-10-02 07:41:30 +0300

    added styles to teams, and runing migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_110924) do
+ActiveRecord::Schema.define(version: 2018_10_02_043514) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
