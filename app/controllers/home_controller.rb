d7aac49c86b2d47ad62852316c133bc68a638128
yachnytskyi <yachnytskyi1992@gmail.com>
2018-10-04 23:16:24 +0300

    Final step.Not saved home_controller.rb (now saved)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
-lass ApplicationController < ActionController::Base
-  include PublicActivity::StoreController
+class HomeController < ApplicationController
+  def index
+    if user_signed_in?
+      @teams = Team.where('id = ?', current_user.team_id)
+      @projects = Project.where('team_id = ?', current_user.team_id)
+    end
+
+    @activities = PublicActivity::Activity.order("created_at DESC").where(owner_id: current_user, owner_type: "User")
+  end
 end
